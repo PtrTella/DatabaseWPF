d416ec2a2a3f15b96258c1781fc3a1ca0961c4d7
PtrTella <tellarinipietro@gmail.com>
2022-08-27 14:44:32 +0200

    WPF project creation

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF
+{
+    /// <summary>
+    /// Logica di interazione per MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            WPF.DatabaseDataSet databaseDataSet = ((WPF.DatabaseDataSet)(this.FindResource("databaseDataSet")));
+            // Carica i dati nella tabella GIOCATORE. Se necessario, è possibile modificare questo codice.
+            WPF.DatabaseDataSetTableAdapters.GIOCATORETableAdapter databaseDataSetGIOCATORETableAdapter = new WPF.DatabaseDataSetTableAdapters.GIOCATORETableAdapter();
+            databaseDataSetGIOCATORETableAdapter.Fill(databaseDataSet.GIOCATORE);
+            System.Windows.Data.CollectionViewSource gIOCATOREViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("gIOCATOREViewSource")));
+            gIOCATOREViewSource.View.MoveCurrentToFirst();
+        }
+    }
+}
